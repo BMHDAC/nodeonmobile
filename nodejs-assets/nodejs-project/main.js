50e6605626128a0cfd5d2169150acbe191bcf296
= <=>
2024-04-26 14:31:18 +0700

    change main

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -14,29 +14,39 @@ class serverProvider {
       })
       .listen(port);
     this.servers.set(port, s);
-    rn_bridge.channel.send('Port listening');
+    rn_bridge.channel.send({
+      message: 'Port listening',
+      port: port,
+    });
   }
   deleteServer(port) {
     if (this.servers.has(port)) {
       this.servers.delete(port);
     } else {
-      rn_bridge.channel.send('The port did not exists');
+      rn_bridge.channel.send({
+        message: 'The port did not exists',
+        port: port,
+      });
     }
   }
 }
 const servers = new serverProvider();
 rn_bridge.channel.post('command', {name: 'Open', message: 'Connection Open'});
 rn_bridge.channel.on('message', msg => {
-  rn_bridge.channel.send(msg);
+  rn_bridge.channel.send({
+    message: msg,
+    port: 0000,
+  });
 });
 rn_bridge.channel.on('command', method => {
   if (method.name == 'Create') {
     try {
       servers.createServer(method.port);
     } catch (error) {
-      rn_bridge.channel.send(
-        'This port is already taken, please change port and try again',
-      );
+      rn_bridge.channel.send({
+        message: 'This port is already taken, please change port and try again',
+        port: port,
+      });
     }
   } else if (method.name == 'Delete') {
     servers.deleteServer(method.port);
